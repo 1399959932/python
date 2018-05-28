03398501e8240e576a4169759425a671001ef71f
Chen Ronin <1399959932@qq.com>
2018-05-28 22:26:18 +0800

    5.28

---
+++ day35.py
@@ -0,0 +1,33 @@
+#/usr/bin/python
+常用第三方模块
+	PIL：Python Imaging Library，已经是Python平台事实上的图像处理标准库了。PIL功能非常强大，
+	Pillow是支持python3的模块
+
+操作图像
+	#图像缩放操作   day35_Pillow.py
+	#运行 cmd下
+	Original image Size:
+	Resize image to :
+
+模糊操作
+	#day35_mohu.py
+	#cmd
+	无反应
+
+生成验证码
+	#day35_code.py
+	#cmd
+	py C:\Users\Ronin\AppData\Local\Programs\Python\Python36-32\practice.py\Day
+
+
+requests
+	# 使用get访问一个页面
+	# 
+
+
+virtualenv	
+	在开发Python应用程序的时候，系统安装的Python3只有一个版本：3.4。所有第三方的包都会被pip安装到Python3的site-packages目录下。
+	每个应用可能需要各自拥有一套“独立”的Python运行环境。virtualenv就是用来为一个应用创建一套“隔离”的Python运行环境。
+
+	# 安装	
+	pip3 install virturalenv
